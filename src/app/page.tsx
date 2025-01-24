a7a07cbff22080402d022fc8d55e0d057c38e2c1
Douglas Socorro <juancarlossocorro571@gmail.com>
2025-01-24 19:26:56 -0400

    feat: start working on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,204 @@
 import Image from "next/image";
 
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { MdLocationOn } from "react-icons/md";
+
+
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col h-[100vh]">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <Header/>
+
+      <main className="grow-[1] bg-blue-600 flex justify-center items-center">
+        <div className="text-neutral-100 text-center">
+          <h1 className="text-3xl font-bold">
+          Servicio Desconcentrado Municipal de
+            Administración Tributaria
+          </h1>
+
+          <h2 className="text-2xl font-bold">
+            Oficina Principal: Delicias 2, Alcaldía del Municipio Zamora, plnata baja.
+          </h2>
         </div>
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      <Footer/>
     </div>
   );
 }
+
+
+interface ITitleProps { 
+  level?: 1 | 2 | 3,
+  mode?: 'light' | 'dark' | 'auto';
+}
+function Title({level, mode, children}: ITitleProps) {
+
+  if (!mode) {
+    mode = 'light'
+  }
+
+  let className = 'text-slate-900'
+
+  if (mode === 'dark') {
+    className += 'text-neutral-100'
+  }
+  
+  if (level === 1) {
+    return <h1 className={className}>
+      {children}
+    </h1>
+  }
+
+  if (level === 2) {
+    return <h2 className={className}>
+      {children}
+    </h2>
+  }
+
+  if (level === 3) {
+    return <h3 className={className}>
+      {children}
+    </h3>
+  }
+
+  return <h1 className={className}>
+    {children}
+  </h1>
+}
+
+
+function Header() {
+  return (<>
+    <header className="
+        p-2      
+        flex
+        items-center
+        shadow-lg
+        "
+      >
+        <div className="flex-1">
+          <Image
+            alt="sedemat zamora logo"
+            src={'/images/sedemat_dark_logo.png'}
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="flex-1 flex justify-end">
+          <AiOutlineMenu 
+            className="m-2"
+            size={'2rem'}
+          />
+        </div>
+      </header>
+  </>)
+}
+
+function Footer(props) {
+
+  const className = `
+    bg-neutral-800
+
+    p-5
+    text-neutral-100
+
+    flex
+
+    justify-center
+    items-center
+  `
+  return (<>
+    <footer 
+
+      {...props}
+    
+      className={props.className + ' ' + className}
+      >
+
+      <div className="
+        max-w-[60%]
+        flex
+        flex-col
+        items-center
+        justify-center
+
+        gap-16
+
+        lg:flex-row
+      ">
+        <div className="flex flex-col items-center">
+          <Image
+            alt="logo del municipio zamora"
+            src={'/images/alcaldia_zamora_logo.png'}
+            width={100}
+            height={100}
+          />
+
+          <p>
+            En nuestra alcaldía, la transparencia es nuestra prioridad. Accede a la información pública de manera clara y accesible
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <h1 className="text-3xl font-bold">
+            Contacto
+          </h1>
+          
+          <h2 className="text-2xl font-bold">
+            Horario
+          </h2>
+          <p>
+            Lunes a viernes 8:00 a.m. a 3:00 p.m.
+          </p>
+
+          <h2 className="text-2xl font-bold">
+            Dirección
+          </h2>
+          <p className="flex">
+            <div className="flex items-center">
+              <MdLocationOn size={'30px'}/>
+            </div>
+            <a href="https://maps.app.goo.gl/ksm4uTyyraLt9sD86" target="_blank" className="flex items-center"> Oficina Principal: Delicias 2, Alcaldía del Municipio Zamora, plnata baja.</a>
+          </p>
+
+          <h2 className="flex gap-1 items-center text-2xl font-bold">
+            <AiOutlineWhatsApp size={'30px'}/>
+
+            <a href='https://wa.me/+584126743853' target="_blank">Whatsapp: +58 412-6743853</a>
+          </h2>
+
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h1 className="text-3xl font-bold">
+            Siguenos en nuestras redes
+          </h1>
+
+          <div className="flex gap-5 justify-center">
+            
+            <a href={SEDEMAT_ZAMORA_FB} target="_blank">
+              <BsFacebook size={'1.5rem'}/>
+            </a>
+            <a href={SEDEMAT_ZAMORA_IG} target="_blank"><BsInstagram  size={'1.5rem'}/></a>
+          </div>
+
+        </div>
+      </div>
+
+      
+
+      </footer>
+  
+  </>)
+}
+
+
+const SEDEMAT_ZAMORA_IG = 'https://www.instagram.com/sedemat.zamora/'
+const SEDEMAT_ZAMORA_FB = 'https://www.facebook.com/sedemat.zamora.5'
