21a3ade4a3244dc800dc46407d8b4b5b963a4c13
Douglas Socorro <juancarlossocorro571@gmail.com>
2025-04-04 06:09:17 -0400

    add: enable news posts

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import { getNewsList } from "../util"
+
+
 async function Article({
   params
 }: {
@@ -5,13 +8,25 @@ async function Article({
 }) {
 
   const { slug } = await params
+  const { default: Content } = await import(`@/app/content/news/${slug}`)
 
   return (
     <>
       <h1>Noticias</h1>
       <p>slug: {slug}</p>
+      <Content/>
     </>
   )
+}
+
+export async function generateStaticParams() {
+  const newsList = getNewsList()
+
+  console.log({newsList})
+
+  return newsList.map((news) => ({
+    slug: news.slug,
+  }))
 }
 
 export default Article
