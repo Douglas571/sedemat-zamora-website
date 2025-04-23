9fb12a0bc38e5305a88e71540fb4313d1b01fa7d
douglas571 <juancarlossocorro571@gmail.com>
2025-04-23 12:39:11 -0400

    fix: remove custom build dir

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: 'build',
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 
   typescript: {
